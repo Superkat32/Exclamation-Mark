cdd99014524bad1d2da469f882cf7b0f9158ce85
Superkat32 <superkat1806@gmail.com>
2022-10-08 14:32:13 -0400

    Initial commit.

---
+++ src/main/java/net/superkat/camera_raindrops/CameraRaindrops.java
@@ -0,0 +1,15 @@
+package net.superkat.camera_raindrops;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CameraRaindrops implements ModInitializer {
+
+	public static final String MOD_ID = "camera_raindrops";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/superkat/camera_raindrops/CameraRaindropsClient.java
@@ -0,0 +1,10 @@
+package net.superkat.camera_raindrops;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CameraRaindropsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
