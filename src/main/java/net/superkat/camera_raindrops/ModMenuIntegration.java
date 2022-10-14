a07c11e02fe8d03b6ec67e8dbe0101131cd93935
Superkat32 <superkat1806@gmail.com>
2022-10-13 23:57:14 -0400

    Literally just making a commit to have a backup in case I need to revert changes later. I'm saving the prototype version of the raindrops.png as well just to have it.

---
+++ src/main/java/net/superkat/camera_raindrops/ModMenuIntegration.java
@@ -0,0 +1,14 @@
+//package net.superkat.camera_raindrops;
+//
+//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+//import com.terraformersmc.modmenu.api.ModMenuApi;
+//
+//public class ModMenuIntegration implements ModMenuApi {
+//    @Override
+//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+//        return parent -> {
+//            // Return the screen here with the one you created from Cloth Config Builder
+//        };
+//    }
+//
+//}
--- src/main/java/net/superkat/camera_raindrops/mixin/ExampleMixin.java
+++ src/main/java/net/superkat/camera_raindrops/mixin/ExampleMixin.java
@@ -1,16 +1,37 @@
 package net.superkat.camera_raindrops.mixin;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.util.Identifier;
 import net.superkat.camera_raindrops.CameraRaindrops;
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		CameraRaindrops.LOGGER.info("This line is printed by an example mod mixin!");
+@Mixin(InGameHud.class)
+public abstract class ExampleMixin {
+
+	private static final Identifier BONK = new Identifier("camera_raindrops:textures/camera_raindrops.png");
+	private static final Identifier TEST = new Identifier("textures/misc/powder_snow_outline.png");
+	private MinecraftClient client;
+
+	@Shadow protected abstract void renderOverlay(Identifier texture, float opacity);
+
+	@Shadow protected abstract void renderSpyglassOverlay(float scale);
+
+	boolean sendMixinMessage = true;
+
+	@Inject(at = @At("HEAD"), method = "render")
+	public void renderOverlay(CallbackInfo info) {
+		if (!this.client.options.getPerspective().isFirstPerson()) {
+//				this.renderOverlay(BONK, 1.0F);
+				this.renderOverlay(BONK, 1.0F);
+				if (sendMixinMessage) {
+					CameraRaindrops.LOGGER.info("This line is printed by an example mod mixin!");
+					sendMixinMessage = false;
+				}
+		}
 	}
 }
