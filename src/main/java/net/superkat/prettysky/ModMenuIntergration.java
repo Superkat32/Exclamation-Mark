0a174a6fd9909abf1139cdf5cdc3277c94518f68
Superkat32 <superkat1806@gmail.com>
2022-10-15 00:01:07 -0400

    Re-added ModMenuIntergration.java because I'll most likely need it later. I'm not sure why the commit action was bugging out, but I seem to have fixed it by clicking the very blue "resolve" button. Wow!

---
+++ src/main/java/net/superkat/prettysky/ModMenuIntergration.java
@@ -0,0 +1,14 @@
+//package net.superkat.prettysky;
+//
+//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+//import com.terraformersmc.modmenu.api.ModMenuApi;
+//
+//public class ModMenuIntegration implements ModMenuApi {
+//    @Override
+//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+//        return parent -> {
+//            // Return the screen here with the one you created from Cloth Config Builder
+//        };
+//    }
+//
+//}
